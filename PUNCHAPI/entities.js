ee5a0bc02cc18cb8c3ecd78866d4951d7fcabb5f
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-10-18 14:10:54 +0000

    commentedentities

--- PUNCHAPI/entities.js
+++ PUNCHAPI/entities.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+/***
+ * Documents which describe users
+ * name: String which represents the name of the user.
+ * token: The token value for this user
+ * gender: String with a single character m, f or o. These character stand for male, female or other respectively.
+ */
 const UsersScheme = new mongoose.Schema({
     name: {
       type: String,
@@ -15,6 +21,13 @@ const UsersScheme = new mongoose.Schema({
       required: true
     }
 });
+
+
+/**
+ * Documents representing companies that have been added to the system and can give out punch cards.
+ * name: String for the company name
+ * punchCount: The number of punches a user needs to obtain in order to be given a discount
+ */
 const CompaniesScheme = new mongoose.Schema({
   name: {
     type: String,
@@ -27,6 +40,14 @@ const CompaniesScheme = new mongoose.Schema({
   }
 });
 
+
+/**
+ * Documents which represents punches
+ * company_id: company id
+ * user_id: user id
+ * created: time stamp when this punch was created
+ * used: this value indicates if the user has used up the discount given to him/her after reaching the punchCount for the given company. 
+ */
 const PunchSchema = new mongoose.Schema({
     created: {
       type: Date,
@@ -46,9 +67,7 @@ const PunchSchema = new mongoose.Schema({
 
 
 const UserEntity = mongoose.model("Users", UsersScheme);
-
 const PunchEntity = mongoose.model("Punches", UsersScheme);
-
 const CompanyEntity = mongoose.model("Companies", CompaniesScheme);
 
 const entities = {
